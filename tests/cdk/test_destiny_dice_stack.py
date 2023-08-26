4bce2751a0f362945610dc6ec79a2679dc419d88
tsuji-tomonori <160441107@ccalumni.meijo-u.ac.jp>
2023-08-27 03:24:25 +0900

    add snapshot test

---
+++ tests/cdk/test_destiny_dice_stack.py
@@ -0,0 +1,19 @@
+import aws_cdk as cdk
+from aws_cdk import assertions
+from syrupy.matchers import path_type
+
+from cdk.destiny_dice_stack import DestinyDiceStack
+
+
+def test_snapshot(snapshot) -> None:
+    app = cdk.Stack()
+    stack = DestinyDiceStack(app, "test")
+    template = assertions.Template.from_stack(stack)
+
+    matcher = path_type(
+        {
+            r".*\.S3Key$": (str,),
+        },
+        regex=True,
+    )
+    assert template.to_json() == snapshot(matcher=matcher)
